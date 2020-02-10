deb4bd0b30e6123395df967a591c5cea950edb1c
Tory BYU <torys.anderson@gmail.com>
2020-02-10 13:58:17 -0700

    v8 multiple sort

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject byu-odh/retabled "7"
+(defproject byu-odh/retabled "8"
   :description "Generate reagent-based tables for display and interaction with data collections"
   :url "http://tech.toryanderson.com"
   :license {:name "Eclipse Public License"
